fc6268e809d9db8a71d7961e64650760a25c8680
piglovesyou <thepiglovesyou@gmail.com>
2019-10-25 21:42:21 +0900

    remove "fromIter": it landed in v10 (v10.17.0)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import fromIter from "./fromIter";
 import split from "./split";
 import pipeline from "./pipeline";
 import ParallelTransform from "./ParallelTransform";
@@ -8,10 +7,8 @@ export default pipe;
 export {default as ParallelTransform} from './ParallelTransform';
 export {default as pipeline} from './pipeline';
 export {default as split} from './split';
-export {default as fromIter} from './fromIter';
 
 module.exports = pipe;
 module.exports.ParallelTransform = ParallelTransform;
 module.exports.pipeline = pipeline;
 module.exports.split = split;
-module.exports.fromIter = fromIter;
