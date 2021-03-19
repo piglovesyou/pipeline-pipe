e7d7cd2dc99036394c5fc69ee0e5851d802b20ee
Soichi Takamura <thepiglovesyou@gmail.com>
2021-03-19 20:40:17 +0900

    Ignore new line linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,16 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error', {
-        'argsIgnorePattern': '^_'
-      }
-    ]
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
