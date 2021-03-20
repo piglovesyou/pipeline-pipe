b17755187407e4b833415ca86648cf32f7714bcf
Soichi Takamura <thepiglovesyou@gmail.com>
2021-03-21 00:47:01 +0900

    Improve environment (#38)
    * Update deps
    
    * modify lint-staged
    
    * prettier
    
    * Use GitHub Actions
    
    * Ignore new line linting
    
    * Ignore eslint error in test file
    
    * Badge
    
    Bye Travis

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,11 +20,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error', {
-        'argsIgnorePattern': '^_'
-      }
-    ]
-  }
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
--- src/concat.test.ts
+++ src/concat.test.ts
@@ -1,35 +1,35 @@
 import assert from 'assert';
 import { Readable } from 'stream';
 import { pipeline } from 'readable-stream';
-import { pipe, concat } from "./index";
+import { pipe, concat } from './index';
 
 describe('concat(size)', () => {
   it('concats incoming stream and build array of given size', async function () {
     const expect1: number[][] = [
-      [ 0, 10, 20, 30, 40 ],
-      [ 50, 60, 70, 80, 90 ],
-      [ 100, 110, 120, 130, 140 ],
-      [ 150, 160, 170, 180, 190 ],
-      [ 200, 210, 220 ],
+      [0, 10, 20, 30, 40],
+      [50, 60, 70, 80, 90],
+      [100, 110, 120, 130, 140],
+      [150, 160, 170, 180, 190],
+      [200, 210, 220],
     ];
-    const expect2: number[] = [ 100, 200 ];
+    const expect2: number[] = [100, 200];
     const actual1: number[][] = [];
     const actual2: number[] = [];
 
     await new Promise<void>((resolve, reject) => {
       pipeline(
-          Readable.from(Array.from(Array(23)).map((_, i) => i)),
-          pipe((n) => n * 10),
-          concat(5),
-          pipe((packed) => {
-            actual1.push(packed);
-            return packed;
-          }),
-          pipe((packed) => packed[0]),
-          concat(3),
-          pipe((packed) => packed[packed.length - 1]),
-          pipe((packed) => actual2.push(packed)),
-          (err) => err ? reject(err) : resolve(),
+        Readable.from(Array.from(Array(23)).map((_, i) => i)),
+        pipe((n) => n * 10),
+        concat(5),
+        pipe((packed) => {
+          actual1.push(packed);
+          return packed;
+        }),
+        pipe((packed) => packed[0]),
+        concat(3),
+        pipe((packed) => packed[packed.length - 1]),
+        pipe((packed) => actual2.push(packed)),
+        (err) => (err ? reject(err) : resolve()),
       );
     });
     assert.deepStrictEqual(actual1, expect1);
